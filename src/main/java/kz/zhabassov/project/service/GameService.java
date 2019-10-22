eb0a4336a83f0a5cecfef3791db55b9e65775def
Tamerlan Zhabassov <zhbssv@mail.ru>
2019-10-22 09:21:03 +0600

    add project

---
+++ src/main/java/kz/zhabassov/project/service/GameService.java
@@ -0,0 +1,39 @@
+package kz.zhabassov.project.service;
+
+import kz.zhabassov.project.dao.GameDao;
+import kz.zhabassov.project.entity.Game;
+import kz.zhabassov.project.entity.Team;
+
+import java.util.List;
+
+public class GameService {
+
+    private GameDao gameDao;
+    //get first 5 with host team and score, guest team and score
+    public List<Game>  showLastFiveGames(){
+        return gameDao.findLastFive();
+    }
+
+    //get all
+    public List<Game> showAllgames(){
+        return gameDao.findAll();
+    }
+
+    //update games with score
+    public int updateScore(Game game){
+        return gameDao.update(game);
+    }
+
+    //add game
+    public int addGame(Game game){
+        return  gameDao.insert(game);
+    }
+
+    public List<Game> showFirstFive(){
+        return gameDao.findFirstFiveWithMaxScore();
+    }
+
+    public void setGameDao(GameDao gameDao) {
+        this.gameDao = gameDao;
+    }
+}
